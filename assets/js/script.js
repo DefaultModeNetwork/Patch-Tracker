8b7ed7ed086d274967875db8fc8340d759261f5d
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-03-12 17:09:40 -0500

    youtube api script

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,35 @@ function renderGameInfo(title, contents, url) {
 
 // callAppIdApi();
 callSteamNewsAPI();
+
+
+
+
+
+  function loadClient() {
+    gapi.client.setApiKey("YOUR_API_KEY");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.channels.list({
+      "part": [
+        "string"
+      ],
+      "id": [
+        "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+      ]
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: ""});
+  });
+
+<><button onclick="authenticate().then(loadClient)">authorize and load</button><button onclick="execute()">execute</button></></>
